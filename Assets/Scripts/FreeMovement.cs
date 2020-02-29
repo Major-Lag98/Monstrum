df5d02eccae76362019d58cbab3463d941120e1d
Alden <aldensmith06@Gmail.com>
2020-02-28 21:01:49 -0800

    fixed movement bug, added house

--- Assets/Scripts/FreeMovement.cs
+++ Assets/Scripts/FreeMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FreeMovement : MonoBehaviour
 {
 
-    Vector2 position;
+    Vector2 move;
     Vector2 lookDirection = Vector2.down;
 
     public float speed = 1;
@@ -25,45 +25,51 @@ public class FreeMovement : MonoBehaviour
     void Update()
     {
         bool walking = false;
-        
+        move = Vector2.zero;
+
         if (Input.GetKey(KeyCode.A))
         {
-            position += Vector2.left * speed * Time.deltaTime;
+            move += Vector2.left;
             lookDirection += Vector2.left;
             walking = true;
 
         }
         if (Input.GetKey(KeyCode.D))
         {
-            position += Vector2.right * speed * Time.deltaTime;
+            move += Vector2.right;
             lookDirection += Vector2.right;
             walking = true;
 
         }
         if (Input.GetKey(KeyCode.W))
         {
-            position += Vector2.up * speed * Time.deltaTime;
+            move += Vector2.up;
             lookDirection += Vector2.up;
             walking = true;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            position += Vector2.down * speed * Time.deltaTime;
+            move += Vector2.down;
             lookDirection += Vector2.down;
             walking = true;
         }
+
+        move.Normalize();
 
         lookDirection.Normalize();
         animator.SetFloat("Look X", lookDirection.x);
         animator.SetFloat("Look Y", lookDirection.y);
         animator.SetBool("Is Walking", walking);
 
-        Debug.Log(walking);
-        Debug.Log(rb.velocity);
+       
     }
 
     private void FixedUpdate()
     {
+        Vector2 position = rb.position;
+
+        position = position + move * speed * Time.deltaTime;
+
         rb.MovePosition(position);
     }
 }
